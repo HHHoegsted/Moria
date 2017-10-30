9623d75cff018b7fc32555cd5930daac6cc3848b
Hans-Henrik Høgsted <hh.hoegsted@gmail.com>
2017-10-30 17:09:57 +0100

    Opstart

---
+++ Moria/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moria
+{
+    class Room
+    {
+        public string location;
+
+        public Room(string aLocation)
+        {
+            location = aLocation;
+        }
+    }
+}
