42224dcb46b6c81b3ca4e456a49276954e044aa4
Hans-Henrik Høgsted <hh.hoegsted@gmail.com>
2017-11-02 20:00:37 +0100

    Mobs and very simple combat

--- Moria/GameEngine.cs
+++ Moria/GameEngine.cs
@@ -9,6 +9,7 @@ namespace Moria
     class GameEngine
     {
         public Player player1;
+        public Mob troll;
         public Item item1;
         public Room currentRoom;
         public Room middleRoom;
@@ -51,6 +52,10 @@ namespace Moria
                         }
                         
                     }
+                    else if (currentRoom.status == "mob")
+                    {
+                        Console.WriteLine("you can't leave, a mob is fighting you!");
+                    }
                     else if (currentRoom.neighbourRight != null)
                     {
                         currentRoom = currentRoom.neighbourRight;
@@ -69,9 +74,14 @@ namespace Moria
                         {
                             Console.WriteLine("You have a key");
                             currentRoom.status = "";
+                            currentRoom.description = "Left room";
                             currentRoom = currentRoom.neighbourLeft;
                             currentRoom.ShowDescription();
                         }
+                    }
+                    else if (currentRoom.status == "mob")
+                    {
+                        Console.WriteLine("You can't leave, a mob is fighting you!");
                     }
                     else if (currentRoom.neighbourLeft != null)
                     {
@@ -88,6 +98,31 @@ namespace Moria
                 else if (command.Equals("quit"))
                 {
                     break;
+                }
+                else if (command.Equals("fight"))
+                {
+                    while (troll.life > 0 && player1.life > 0)
+                    {
+                        Console.WriteLine("The troll hits you, and you fight back");
+                        player1.life -= 10;
+                        troll.life -= 20;
+                        Console.WriteLine("your health is {0} and the trolls' health is {1}", player1.life, troll.life);
+                    }
+
+                    if (player1.life < 0)
+                    {
+                        Console.WriteLine("You are dead, game is over");
+                        Console.ReadKey();
+                        break;
+                    }
+
+                    if (troll.life < 0)
+                    {
+                        Console.WriteLine("You are victorious and the troll is dead");
+                        currentRoom.status = "";
+                        leftLeftRoom.description = "The far left room, there is a dead troll on the floor";
+                    }
+                    
                 }
                 else if (command.Equals("search"))
                 {
@@ -129,13 +164,15 @@ namespace Moria
         public void InstantiateObjects()
         {
             player1 = new Player("HH", 100);
+            troll = new Mob("Troll", 50);
             item1 = new Item("a key");
             middleRoom = new Room("Middle", "Starting Room");
             middleRoom.item = item1;
             leftRoom = new Room("Left", "Left Room, the door to the left is locked");
             leftRoom.status = "LockedLeft";
             rightRoom = new Room("Right", "Right Room");
-            leftLeftRoom = new Room("Far Left", "Left room behind a locked door");
+            leftLeftRoom = new Room("Far Left", "Behind the locked door is a troll, and it attacks you!");
+            leftLeftRoom.status = "mob";
 
         }
     }
---
+++ Moria/Mob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moria
+{
+    class Mob
+    {
+        public string name;
+        public int life;
+
+        public Mob(string aName, int lifeAmount)
+        {
+            name = aName;
+            life = lifeAmount;
+        }
+    }
+}
